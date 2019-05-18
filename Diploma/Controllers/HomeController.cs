86d29096a6a761f96d2da7faa5c1885511ee4c4c
Artur <imbalance1997@gmail.com>
2019-05-18 13:55:25 +0300

    news pagination

--- Diploma/Controllers/HomeController.cs
+++ Diploma/Controllers/HomeController.cs
@@ -5,16 +5,44 @@ using System.Web;
 using System.Web.Mvc;
 using Diploma.Models;
 using System.Data.Entity;
+using PagedList.Mvc;
+using PagedList;
 
 namespace Diploma.Controllers
 {
+    [RoutePrefix("home")]
     public class HomeController : Controller
     {
         ApplicationContext db = new ApplicationContext();
 
+        [Route("~/home")]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Route("/allnews/{page}")]
+        public ActionResult AllNews(int? page)
+        {
+            //Количество объектов на страницу
+            int pageSize = 3;
+
+            //Количество страниц
+            int pageNumber;
+
+            if (page == null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                pageNumber = (int)page;
+            }
+
+            //Все новости
+            var news = db.News.Include(n => n.Category).ToList();
+
+            return View(news.ToPagedList(pageNumber, pageSize));
         }
     }
 }
